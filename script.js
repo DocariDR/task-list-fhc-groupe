ea75d0d674edfe20f8975add1c0eabc01e32268f
DocariDR <ricardovonoupro@gmail.com>
2026-08-01 17:17:15 +0100

    feat(js): crée dynamiquement un élément de tâche à partir du champ de saisie

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ form.addEventListener("submit", fetchValue)
 
 function fetchValue(event) {
     event.preventDefault();
+    const input = document.querySelector(".input-field");
+    const listItem = document.createElement("li")
+    const value = input.value;
+    listItem.innerHTML = `<div>${value}</div><button type="button" class="delete-btn"><span class="material-symbols-outlined">delete</span></button>`
+    listItem.className = "task-item"
+    list.appendChild(listItem)
+    input.value = ""
 }
